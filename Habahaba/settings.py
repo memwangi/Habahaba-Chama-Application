3f18382f36c902ab4c0d7fc2bf3f404884ec6fb0
memwangi <donmwas007@gmail.com>
2019-04-20 09:47:32 +0300

    Configured env files

--- Habahaba/settings.py
+++ Habahaba/settings.py
@@ -202,6 +202,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'donmwas007@gmail.com'
-EMAIL_HOST_PASSWORD = 'xldmrkoamluxavsf'
+EMAIL_HOST_USER = 'EMAIL_HOST_USER'
+EMAIL_HOST_PASSWORD = 'EMAIL_HOST_PASSWORD'
 django_heroku.settings(locals())
